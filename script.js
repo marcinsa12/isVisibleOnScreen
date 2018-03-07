87c9680345d6558c5b2a8594864a17ef1411c190
marcinsa12 <marcinsa12@gmail.com>
2018-03-07 19:44:18 +0000

    Update script.js
    Fixed

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
- function isVisibleOnScreen(el)
- {
-     var screenTop = window.scrollY;
-     var screenBottom = screenTop + window.innerHeight;
-     var elementTop = el.offsetTop;
-     var elementBottom = elementTop + el.offsetHeight;
-     return ((screenTop <= elementBottom) && (screenBottom >= elementTop) ); //gives TRUE when even small part of the element is on the screen.
-   //  return ((elementBottom <= screenBottom) && (elementTop >= screenTop)); // gives TRUE when whole element is visible on the screen.
+function getPosition(elem) {
+  var distanceFromTop = 0;
+  while(elem) {
+    distanceFromTop += (elem.offsetTop + elem.clientTop);
+    elem = elem.offsetParent;
+  }
+  return distanceFromTop;
 }
- 
+//This tells us when element is even partially visible on the screen
+function isVisibleOnScreen(elem) {
+    var screenTop = /MSIE \d|Trident.*rv:/.test(navigator.userAgent) ? document.querySelector('html').scrollTop : window.scrollY;
+    var screenBottom = screenTop + window.innerHeight;
+    var elemTop = getPosition(elem);
+    var elemBottom = elemTop + elem.offsetHeight;
+    return ((screenTop <= elemBottom) && (screenBottom >= elemTop) );
+}
+
+//This tells us when element is visible on the screen from top til bottom
+function isFullyVisibleOnScreen(elem) {
+    var screenTop = /MSIE \d|Trident.*rv:/.test(navigator.userAgent) ? document.querySelector('html').scrollTop : window.scrollY;
+    var screenBottom = screenTop + window.innerHeight;
+    var elemTop = getPosition(elem);
+    var elemBottom = elemTop + elem.offsetHeight;
+    return ((elemBottom <= screenBottom) && (elemTop >= screenTop));
+}
